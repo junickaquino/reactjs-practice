ce347087972e056aec1338f2e37da07a65517d93
junickaquino <junickaquino29@gmail.com>
2022-04-07 02:09:42 +0800

    Displaying multiple elements within one div

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 ReactDOM.render(
-  <ul>
-    <li>Hello</li>
-    <li>Kamusta</li>
-  </ul>,
+  <div>
+    <h1>Hello, Madlang People!</h1>
+    <p>Welcome po kayo.</p>
+
+    <ul>
+      <li>Hello</li>
+      <li>Kamusta</li>
+    </ul>
+  </div>,
   document.querySelector("#root")
 );
