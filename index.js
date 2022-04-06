9229cc710b0d4498b1d72ea938d7ac629908e12b
junickaquino <junickaquino29@gmail.com>
2022-04-07 02:20:19 +0800

    Creating a custom component (MainContent)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-ReactDOM.render(
-  <div>
-    <h1>Hello, Madlang People!</h1>
-    <p>Welcome po kayo.</p>
+function MainContent() {
+  return (
+    <div>
+      <h1>Hello, Madlang People!</h1>
+      <p>This is the main content</p>
 
-    <ul>
-      <li>Hello</li>
-      <li>Kamusta</li>
-    </ul>
-  </div>,
-  document.querySelector("#root")
-);
+      <ul>
+        <li>Hindi ba?</li>
+        <li>Kamusta</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(<MainContent />, document.querySelector("#root"));
