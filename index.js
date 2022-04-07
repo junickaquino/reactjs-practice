a21aac493b9ead2e8f223e1c5186cc7bb09b774a
junickaquino <junickaquino29@gmail.com>
2022-04-07 09:33:56 +0800

    Reasons why I am excited to be learning React part 2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function CustomPage() {
   return (
     <div>
+      <header>
+        <nav>
+          <img src="./react-logo.svg" width="50px"></img>
+        </nav>
+      </header>
+
       <h1>Reasons why I am excited to be learning React</h1>
       <ol>
         <li>Find a job or career using React</li>
         <li>Expand my skills</li>
         <li>Actually become a Full Stack developer</li>
       </ol>
+
+      <footer>
+        <small>@ 2022 JunickAquino development. All rights reserved.</small>
+      </footer>
     </div>
   );
 }
