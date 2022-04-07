9babadc742b95b31acdfd349bb2bd3867f591ceb
junickaquino <junickaquino29@gmail.com>
2022-04-07 09:26:53 +0800

    Reasons why I am excited to be learning React

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-// function fixLogoBg() {
-//   const logo = document.getElementById("react-logo");
-//   logo.style.width = "90px";
-// }
-
-function FunFacts() {
+function CustomPage() {
   return (
     <div>
-      <img id="react-logo" src="./react-logo.svg" width="40px" />
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <h1>Reasons why I am excited to be learning React</h1>
+      <ol>
+        <li>Find a job or career using React</li>
+        <li>Expand my skills</li>
+        <li>Actually become a Full Stack developer</li>
+      </ol>
     </div>
   );
 }
 
-ReactDOM.render(<FunFacts />, document.getElementById("root"));
-// fixLogoBg();
+const root = document.getElementById("root");
+
+ReactDOM.render(<CustomPage />, root);
