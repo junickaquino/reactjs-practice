974ce06a0429506f5b3ca991797f15d8b429689f
junickaquino <junickaquino29@gmail.com>
2022-04-07 10:24:30 +0800

    Organizing Components

---
+++ Footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <footer>
+      <small>@ 2022 JunickAquino development. All rights reserved.</small>
+    </footer>
+  );
+}
--- index.js
+++ index.js
@@ -1,38 +1,4 @@
-function Header() {
-  return (
-    <header>
-      <nav className="navbar">
-        <img className="logo" src="./react-logo.svg"></img>
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <div className="main-content">
-      <h1>Reasons why I am excited to be learning React</h1>
-      <ol>
-        <li>Find a job or career using React</li>
-        <li>Expand my skills</li>
-        <li>Actually become a Full Stack developer</li>
-      </ol>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>@ 2022 JunickAquino development. All rights reserved.</small>
-    </footer>
-  );
-}
+// import { Header } from "./Header";
 
 function CustomPage() {
   return (
@@ -47,13 +13,5 @@ function CustomPage() {
 }
 
 const root = document.getElementById("root");
-
-// ReactDOM.render(
-//   <div>
-//     <Header />
-//     <CustomPage />
-//   </div>,
-//   root
-// );
 
 ReactDOM.render(<CustomPage />, root);
