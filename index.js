cba4ff9e390b53f6ee0b3cef9bc02464e53ac0d1
junickaquino <junickaquino29@gmail.com>
2022-04-07 09:47:28 +0800

    Parent-Child Components

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-function CustomPage() {
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="./react-logo.svg" width="50px"></img>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react-logo.svg" width="50px"></img>
-        </nav>
-      </header>
-
       <h1>Reasons why I am excited to be learning React</h1>
       <ol>
         <li>Find a job or career using React</li>
         <li>Expand my skills</li>
         <li>Actually become a Full Stack developer</li>
       </ol>
+    </div>
+  );
+}
 
-      <footer>
-        <small>@ 2022 JunickAquino development. All rights reserved.</small>
-      </footer>
+function Footer() {
+  return (
+    <footer>
+      <small>@ 2022 JunickAquino development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function CustomPage() {
+  return (
+    <div>
+      <Header />
+
+      <MainContent />
+
+      <Footer />
     </div>
   );
 }
 
 const root = document.getElementById("root");
+
+// ReactDOM.render(
+//   <div>
+//     <Header />
+//     <CustomPage />
+//   </div>,
+//   root
+// );
 
 ReactDOM.render(<CustomPage />, root);
