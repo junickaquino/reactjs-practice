06dfdb9e6cdb3dfc59345b66b8d4218269637390
junickaquino <junickaquino29@gmail.com>
2022-04-08 08:26:10 +0800

    React Info Site - Project Setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h1>Navbar Component</h1>;
+}
+
+export default Navbar;
