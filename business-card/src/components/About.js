add0416d2481f7825e92d8f007f95066df604869
junickaquino <junickaquino29@gmail.com>
2022-04-08 12:37:43 +0800

    About component and styling

--- business-card/src/components/About.js
+++ business-card/src/components/About.js
@@ -3,7 +3,12 @@ import "../styles/About.css";
 function About() {
   return (
     <div className="About">
-      <h1>About</h1>
+      <h1 className="about--title">About</h1>
+      <p className="about--description">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
     </div>
   );
 }
