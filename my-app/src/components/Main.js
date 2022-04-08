da660281abae70144e1e71e6c730c22bf087ba77
junickaquino <junickaquino29@gmail.com>
2022-04-08 09:19:44 +0800

    React Info Site - Main Content and Basic Styling

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,5 +1,16 @@
 function Main() {
-  return <h1>Main Component</h1>;
+  return (
+    <div className="main">
+      <h1 className="title">Fun facts about React</h1>
+      <ul className="list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
