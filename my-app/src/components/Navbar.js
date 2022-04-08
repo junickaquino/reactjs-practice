05376d95e3c4d59574826a9b3c34ebfe242b46f0
junickaquino <junickaquino29@gmail.com>
2022-04-08 08:57:23 +0800

    React Info Site - Navbar and styling

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,5 +1,18 @@
+import logo from "../logo.svg";
+
 function Navbar() {
-  return <h1>Navbar Component</h1>;
+  return (
+    <nav>
+      <div className="logo">
+        <img src={logo} className="react-logo" alt="logo" />
+        <h2 className="logo-title">ReactFacts</h2>
+      </div>
+
+      <div className="project-name">
+        <h3>React Course - Project 1</h3>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
