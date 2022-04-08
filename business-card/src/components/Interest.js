838b4be63d61a9f13787468b1374d2ff2be329f0
junickaquino <junickaquino29@gmail.com>
2022-04-08 12:39:29 +0800

    Interests component and styling

--- business-card/src/components/Interest.js
+++ business-card/src/components/Interest.js
@@ -3,7 +3,11 @@ import "../styles/Interest.css";
 function Interest() {
   return (
     <div className="Interest">
-      <h1>Interest</h1>
+      <h1 className="interest--title">Interests</h1>
+      <p className="interest--description">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
     </div>
   );
 }
