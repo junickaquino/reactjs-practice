9506a88ebd340b7a0a4c6d4ff00012117470a2e0
junickaquino <junickaquino29@gmail.com>
2022-04-09 17:04:32 +0800

    Meme Generator - get random meme onclick

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,9 +1,19 @@
 import "../styles/Meme.css";
 
+import memesData from "../memesData";
+
 function Meme() {
+  function getRandomMeme() {
+    const memesArray = memesData.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArray.length);
+    const url = memesData.data.memes[randomNum].url;
+
+    console.log(url);
+  }
+
   return (
     <div className="meme">
-      <form className="form" action="">
+      <div className="form" action="">
         <div className="form--text-inputs">
           <input
             className="form--input-text"
@@ -18,10 +28,11 @@ function Meme() {
         </div>
         <input
           className="form--input-submit"
+          onClick={getRandomMeme}
           type="submit"
           value="Get a new meme image 🖼"
         />
-      </form>
+      </div>
     </div>
   );
 }
